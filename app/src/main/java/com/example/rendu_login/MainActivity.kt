443a5601f28562772aa62d155ff697a9f51d7bc6
Julien Cousin-Alliot <Nisouc@outlook.com>
2022-06-14 12:48:45 +0200

    Fix build version errors

--- app/src/main/java/com/example/rendu_login/MainActivity.kt
+++ app/src/main/java/com/example/rendu_login/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.rendu_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
